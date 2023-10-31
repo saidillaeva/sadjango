cb575094ed2210e68eca665a934d9bfe60d0ca62
saidillaeva <ajdanasajdullaeva5@gmail.com>
2023-11-01 00:02:12 +0600

    'django1'

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . import models
+def hello(request):
+    return HttpResponse('<h1>HelloWorld</h1>')
+
+
+def blog_all(request):
+    post = models.Post.objects.all()
+    return render(request, 'post_list.html', {'post': post})
